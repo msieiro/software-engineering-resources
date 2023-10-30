e651c1cc5df43c2d39472edff9689f78cd9292b3
Martin Sieiro <sieirogutierrezmartin@gmail.com>
2023-10-30 22:30:18 +0100

    ✅ add some tests

--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -14,7 +14,9 @@ export default function ErrorAlert() {
                     d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
             </svg>
-            <span>Error! Could not retrieve results from NotionAPI.</span>
+            <span data-testid="api-error">
+                Error! Could not retrieve results from NotionAPI.
+            </span>
         </div>
     )
 }
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,9 @@
 export default function Loader() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
+        <div
+            className="flex items-center justify-center min-h-screen"
+            data-testid="loader"
+        >
             <span className="loading loading-dots loading-lg"></span>
         </div>
     )
