99160c5fe8f4385182357b43b00d49802133d3ca
Martin Sieiro <sieirogutierrezmartin@gmail.com>
2023-11-07 21:42:44 +0100

    🔧 strict ts, lint cmd in cicd and theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Open Sans'],
+                'sans-bold': ['Open Sans Bold']
+            }
+        }
+    },
     daisyui: {
         base: true,
         styled: true,
         utils: true,
-        themes: ['corporate', 'night']
+        themes: [
+            'light',
+            'dark',
+            'cupcake',
+            'bumblebee',
+            'emerald',
+            'corporate',
+            'synthwave',
+            'retro',
+            'cyberpunk',
+            'valentine',
+            'halloween',
+            'garden',
+            'forest',
+            'aqua',
+            'lofi',
+            'pastel',
+            'fantasy',
+            'wireframe',
+            'black',
+            'luxury',
+            'dracula',
+            'cmyk',
+            'autumn',
+            'business',
+            'acid',
+            'lemonade',
+            'night',
+            'coffee',
+            'winter'
+        ]
     },
     plugins: [require('daisyui')]
 }
