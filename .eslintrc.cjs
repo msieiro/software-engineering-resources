e7e206a705cf628e6a716415808f3105438d2bc8
Martin Sieiro <sieirogutierrezmartin@gmail.com>
2023-11-07 21:42:44 +0100

    🔧 strict ts, lint cmd in cicd and theme config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,24 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:react-hooks/recommended'
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    ignorePatterns: ['dist', 'build', '.eslintrc.js'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+        project: './tsconfig.json'
+    },
+    plugins: ['@typescript-eslint', 'react-refresh'],
     rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/strict-boolean-expressions': 'error',
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true }
-        ]
+        ],
+        '@typescript-eslint/no-unsafe-argument': 'off'
     }
 }
