2ce4101563e295f4607bf72dfb2f2434b036e05e
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2023-11-11 16:33:52 +0100

    ⭐️ feat: Add semver package and versioning script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Software Engineering Resources
+
+
+## Incrementador de Versiones Semánticas
+
+Utiliza el siguiente comando para incrementar la versión según la parte especificada:
+
+```bash
+node versioning.cjs <part>
+```
+
+Sustituye <part> con la parte que deseas incrementar (major, minor o patch).
--- versioning.cjs
+++ versioning.cjs
@@ -1,39 +1,42 @@
 const fs = require('fs')
 const path = require('path')
+const semver = require('semver')
 
 const versionFilePath = path.join(__dirname, 'package.json')
-const versionFile = require(versionFilePath)
 
-function incrementVersion(part) {
-    const currentVersion = versionFile.version
-        .split('.')
-        .map((num) => parseInt(num, 10))
+function readPackageFile() {
+    return require(versionFilePath)
+}
 
-    if (part === 'major') {
-        currentVersion[0] += 1
-        currentVersion[1] = 0
-        currentVersion[2] = 0
-    } else if (part === 'minor') {
-        currentVersion[1] += 1
-        currentVersion[2] = 0
-    } else if (part === 'patch') {
-        currentVersion[2] += 1
+function incrementVersion(currentVersion, part) {
+    return semver.inc(currentVersion, part)
+}
+
+function writePackageFile(versionFile) {
+    fs.writeFileSync(
+        versionFilePath,
+        JSON.stringify(versionFile, null, 2) + '\n'
+    )
+}
+
+function main() {
+    const part = process.argv[2]
+
+    if (!part) {
+        console.error(
+            'Please specify a valid version part to increment: major, minor, patch'
+        )
+        process.exit(1)
     }
 
-    return currentVersion.join('.')
+    const versionFile = readPackageFile()
+    const currentVersion = versionFile.version
+    const newVersion = incrementVersion(currentVersion, part)
+
+    versionFile.version = newVersion
+    writePackageFile(versionFile)
+
+    console.log(`Version updated to: ${newVersion}`)
 }
 
-const part = process.argv[2]
-if (!part) {
-    console.error(
-        'Please specify version part to increment: major, minor, or patch'
-    )
-    process.exit(1)
-}
-
-const newVersion = incrementVersion(part)
-versionFile.version = newVersion
-
-fs.writeFileSync(versionFilePath, JSON.stringify(versionFile, null, 2) + '\n')
-
-console.log(`Version updated to: ${newVersion}`)
+main()
