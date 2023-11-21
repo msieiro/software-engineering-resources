072c76fb3c031aa7cbfd10f19d377b34e080d74f
Martín Sieiro Gutiérrez <sieirogutierrezmartin@gmail.com>
2023-11-21 20:53:30 +0100

    ⬆️ upgrade deps and add million.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react'
+import million from 'million/compiler'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [million.vite({ auto: true }), react()],
     test: {
         environment: 'happy-dom',
         globals: true,
